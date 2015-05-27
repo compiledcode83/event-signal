10b2c3a43820518cadae58d175cc0fae27b9d7e0
residentevil0803 <ramseyklassen0803@gmail.com>
2015-05-27 00:23:41 -0400

    Add source map

--- dist/event-signal.js
+++ dist/event-signal.js
@@ -1,4 +1,4 @@
-/* event-signal v0.1.3 - Thu Apr 23 2015 03:45:35 GMT-0400 (EDT) */
+/* event-signal v0.1.3 - 2015-05-27T04:23:11.862Z - https://github.com/r-park/event-signal */
 (function(root, factory) {
   if (typeof define === 'function' && define.amd) {
     define([], factory);
